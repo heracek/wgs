6800d506d9ec22bf109db3520027c5268a810b68
Tomas Horacek <tomas.horacek@gmail.com>
2008-09-09 20:58:19 +0200

    Uchovavani poradi vytvorenych poli s dotezem. Generovani spravnych id poly dotazu.

--- apps/graphs/forms.py
+++ apps/graphs/forms.py
@@ -1,6 +1,9 @@
 # -*- encoding: utf-8 -*-
 from django import forms
 from django.conf import settings
+from django.utils.html import escape
+from django.utils.encoding import force_unicode
+from django.utils.safestring import mark_safe
 import datetime
 import time
 
@@ -29,14 +32,14 @@ class IntegerQueryWidget(forms.MultiWidget):
             forms.Select(attrs=attrs, choices=operands),
             forms.TextInput(attrs=attrs)
         )
+        
         super(IntegerQueryWidget, self).__init__(widgets, attrs)
         
     def decompress(self, value):
         return ()
     
     def format_output(self, rendered_widgets):
-        return '<span id="%s">%s</span>' % \
-            (self._id, u''.join(rendered_widgets))#, settings.MEDIA_URL, settings.MEDIA_URL)
+        return '<span class="dqf_field_body">%s</span>' % (u''.join(rendered_widgets))
         
 class IntegerQueryField(forms.MultiValueField):
     def __init__(self, required=False, label=None, widget=None, initial=0):
@@ -52,10 +55,8 @@ class IntegerQueryField(forms.MultiValueField):
         )
         
         super(IntegerQueryField, self).__init__(fields, required, widget, label)
-        # from pprint import pprint
-        # pprint(dir(self))
-        widget._id = self.label
-    
+        
+        
     def compress(self, data_list):
         if len(data_list) != 3:
             # print self
@@ -63,13 +64,23 @@ class IntegerQueryField(forms.MultiValueField):
         does_apply = data_list[0] is not None and data_list[2] is not None
         return {'field': data_list[0], 'operator': data_list[1], 'operand': data_list[2], 'does_apply': does_apply}
 
+class DQFFieldsField(forms.CharField):
+    def __init__(self, *args, **kwargs):
+        self.max_length, self.min_length = None, None
+        kwargs['required'] = False
+        kwargs['widget'] = forms.HiddenInput()
+        super(DQFFieldsField, self).__init__(*args, **kwargs)
+    pass
+
+
 class QueryForm(forms.Form):
-    #add_query_field = 
+    fields = DQFFieldsField()
     q_1 = IntegerQueryField()
-    #q_2 = IntegerQueryField()
+    # q_2 = IntegerQueryField()
     
-    ADD_REMOVE_BUTTONS = """<span class="dqf_add_remove_controls right">
-            <img class="dqf_button dqf_remove_button" src="%(MEDIA_URL)simg/dqf/remove.png" alt="remove" width="24" heignt="24" /><img class="dqf_button dqf_add_button" src="%(MEDIA_URL)simg/dqf/add.png" alt="add" width="24" heignt="24" />
+    ADD_REMOVE_BUTTONS = """<span class="dqf_add_remove_controls">
+            <img class="dqf_button dqf_remove_button" src="%(MEDIA_URL)simg/dqf/remove.png" alt="remove" width="24" heignt="24" />""" \
+        """<img class="dqf_button dqf_add_button" src="%(MEDIA_URL)simg/dqf/add.png" alt="add" width="24" heignt="24" />
         </span>"""
     
     def as_dqf_ul(self):
@@ -78,8 +89,53 @@ class QueryForm(forms.Form):
             error_row=u'<li>%s</li>',
             row_ender='</li>',
             help_text_html=u' %s',
-            errors_on_separate_row=False
+            errors_on_separate_row=False,
+            insert_hidden_in_the_last_row=False
         )
+    
+    def _html_output(self, normal_row, error_row, row_ender, help_text_html, errors_on_separate_row, insert_hidden_in_the_last_row=True):
+        "Helper function for outputting HTML. Used by as_table(), as_ul(), as_p()."
+        top_errors = self.non_field_errors() # Errors that should be displayed above all fields.
+        output, hidden_fields = [], []
+        for name, field in self.fields.items():
+            bf = forms.forms.BoundField(self, field, name)
+            bf_errors = self.error_class([escape(error) for error in bf.errors]) # Escape and cache in local variable.
+            if bf.is_hidden:
+                if bf_errors:
+                    top_errors.extend([u'(Hidden field %s) %s' % (name, force_unicode(e)) for e in bf_errors])
+                hidden_fields.append(unicode(bf))
+            else:
+                if errors_on_separate_row and bf_errors:
+                    output.append(error_row % force_unicode(bf_errors))
+                if bf.label:
+                    label = escape(force_unicode(bf.label))
+                    # Only add the suffix if the label does not end in
+                    # punctuation.
+                    if self.label_suffix:
+                        if label[-1] not in ':?.!':
+                            label += self.label_suffix
+                    label = bf.label_tag(label) or ''
+                else:
+                    label = ''
+                if field.help_text:
+                    help_text = help_text_html % force_unicode(field.help_text)
+                else:
+                    help_text = u''
+                output.append(normal_row % {'errors': force_unicode(bf_errors), 'label': force_unicode(label), 'field': unicode(bf), 'help_text': help_text})
+        if top_errors:
+            output.insert(0, error_row % force_unicode(top_errors))
+        if hidden_fields: # Insert any hidden fields in the last row.
+            str_hidden = u''.join(hidden_fields)
+            if output and insert_hidden_in_the_last_row:
+                last_row = output[-1]
+                # Chop off the trailing row_ender (e.g. '</td></tr>') and
+                # insert the hidden fields.
+                output[-1] = last_row[:-len(row_ender)] + str_hidden + row_ender
+            else:
+                # If there aren't any rows in the output, just append the
+                # hidden fields.
+                output.append(str_hidden)
+        return mark_safe(u'\n'.join(output))
     
     class Media:
         js = ('js/jquery.js', 'js/django-query-form.js',)
--- site-media/js/django-query-form.js
+++ site-media/js/django-query-form.js
@@ -8,40 +8,55 @@ function DQFInitializeForm() {
             $(this).click(_dqf_remove_button_click);
         });
         
-        $('#dqf_head_add_button').each(function() {
-            $(this).click(function () {
-                var dqf_fields_ul = $("ul#dqf_fields")[0];
-                _dqf_create_field_li($(dqf_fields_ul), prepend=true)
-            });
-        })
+        $('#dqf_head_add_button:first').click(function () {
+            var dqf_fields_ul = $("ul#dqf_fields")[0];
+            _dqf_create_field_li($(dqf_fields_ul), prepend=true);
+        });
         
-        $('#dqf_head_remove_button').each(function() {
-            $(this).click(function () {
-                $("li.dqf_field", "ul#dqf_fields").remove();
-            });
-        })
+        $('#dqf_head_remove_button:first').click(function () {
+            $("li.dqf_field", "ul#dqf_fields").remove();
+        });
+        
+        $('#dqf_form').submit(function () {
+            dqf_update_fields_field();
+            return true;
+        });
+    }
+    
+    function dqf_get_free_field_name() {
+        for (var i = 0; true; i++) {
+            var widget_id_str = '#id_q_' + i + '_0'
+            if ($(widget_id_str).length == 0) {
+                return 'q_' + i
+            }
+        }
+    }
+    
+    function dqf_update_fields_field() {
+        
+        var field_names_list = [];
+        
+        $('.dqf_field', 'ul#dqf_fields').each(function () {
+            var first_cildren = $('.dqf_field_body:first', this).children()[0];
+            var widget_id = $(first_cildren).attr('id');
+            var splited_id = widget_id.split('_');
+            var field_name = [splited_id[1], splited_id[2]].join('_');
+            field_names_list.push(field_name);
+        });
+        
+        $('#id_fields').attr('value', field_names_list.join(','));
     }
     
     function _dqf_create_field_li(where, prepend) {
-        field_li_text = '<li class="dqf_field"><span class="dqf_add_remove_controls right">' +
+        var field_name = dqf_get_free_field_name()
+        var field_li_text = '<li class="dqf_field"><span class="dqf_add_remove_controls">' +
             '<img width="24" heignt="24" alt="remove" src="/site-media/img/dqf/remove.png" class="dqf_button dqf_remove_button"/>' +
             '<img width="24" heignt="24" alt="add" src="/site-media/img/dqf/add.png" class="dqf_button dqf_add_button"/>' +
-            '</span><span id="None"><select id="id_q_1_0" name="q_1_0">' +
+            '</span><span class="dqf_field_body"><select id="id_' + field_name + '_0" name="' + field_name + '_0">' +
             '<option selected="selected" value=""/>' +
             '<option value="num_components">Number of components</option>' +
             '</select>' +
             '</span></li>';
-            /*            
-                        <select id="id_q_1_1" name="q_1_1">
-                        <option value="=">=</option>
-                        <option selected="selected" value="!=">≠</option>
-                        <option value="<"><</option>
-                        <option value=">">></option>
-                        <option value="<=">≤</option>
-                        <option value=">=">≥</option>
-                        </select><input type="text" id="id_q_1_2" value="12" name="q_1_2"/>
-            */            
-
         
         var added_dqf_field_li;
         
@@ -51,13 +66,9 @@ function DQFInitializeForm() {
             added_dqf_field_li = where.prepend(field_li_text).children()[0];
         }
         
-        $('.dqf_add_button', added_dqf_field_li).each(function(b){
-            $(this).click(_dqf_add_button_click);
-        });
+        $('.dqf_add_button:first', added_dqf_field_li).click(_dqf_add_button_click);
         
-        $('.dqf_remove_button', added_dqf_field_li).each(function() {
-            $(this).click(_dqf_remove_button_click);
-        });
+        $('.dqf_remove_button:first', added_dqf_field_li).click(_dqf_remove_button_click);
     }
     
     function _dqf_add_button_click() {
